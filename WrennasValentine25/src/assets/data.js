0ee948dc6eaaed6bba93d969846f17a90a9efb5b
Jeff Ashear <kinesthesiac@gmail.com>
2025-02-05 16:32:38 -0700

    changes to file structure, troubleshooting delay mechanisms

--- WrennasValentine25/src/assets/data.js
+++ WrennasValentine25/src/assets/data.js
@@ -3,7 +3,7 @@ const data = {
 
     1: [ "It's that time of year, ", "for their daughters so dear", "so wise, and so kind.", "with all of his mind.", "or dancing so classy,", "even when it gets gassy...", "or low on the beach,", "always in reach.", "or far, far away.", "Day after day.", "or when you're quite near,", "year after year", "and always around,", "and small sleepy towns.", "and when we're apart,", "with all of my heart."],
 
-    2: ["./src/assets/first.jpg", "./src/assets/second.jpg", "./src/assets/third.jpg"]
+    2: ["./src/assets/images/one.jpg", "./src/assets/images/two.jpg", "./src/assets/images/three.jpg"]
 }
 
 export default data
