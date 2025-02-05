2b86019c915fc25dbc02057f26cd0062ad281d55
Jeff Ashear <kinesthesiac@gmail.com>
2025-02-05 16:12:40 -0700

    new copy

--- WrennasValentine25/src/assets/data.js
+++ WrennasValentine25/src/assets/data.js
@@ -1,6 +1,8 @@
 const data = {
-    0: ["Happy Valentines Bean!", "When dads are bursting with love", "Playing with you always makes me so happy"],
-    1: [ "It's that time of year, ", "for their daughters so dear", "even when things get a little bit gassy."],
+    0: ["Happy Valentines Bean!", "When dads are bursting with love", "You are so helpful,", "Daddy loves you immensely", "making music with you", "playing with you's THE BEST ", "High in the mountains", "Daddy's love is right with you", "Right in Dad's lap", "Daddy's love is right with you,", "When you are distant,", "Daddy's love will be with you", "Daddy's love is forever", "In big busy cities", "When we're together", "I love you dear Wrenna,"],
+
+    1: [ "It's that time of year, ", "for their daughters so dear", "so wise, and so kind.", "with all of his mind.", "or dancing so classy,", "even when it gets gassy...", "or low on the beach,", "always in reach.", "or far, far away.", "Day after day.", "or when you're quite near,", "year after year", "and always around,", "and small sleepy towns.", "and when we're apart,", "with all of my heart."],
+
     2: ["./src/assets/first.jpg", "./src/assets/second.jpg", "./src/assets/third.jpg"]
 }
 
